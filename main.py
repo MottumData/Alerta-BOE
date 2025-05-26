03fc631aef557008295227548482e57584220793
Arturo <aoa2eso@gmail.com>
2025-05-26 14:14:46 +0200

    Refactorizar la ejecución del script y simplificar el envío de notificaciones

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from codecarbon import OfflineEmissionsTracker
 import time
 from internal.llm_utils import *
 from internal.boe_utils import *
-from internal.notification_utils import send_boe_notification_email, read_json_receivers
+from internal.notification_utils import *
 import json
 from pprint import pprint
 
@@ -48,54 +48,39 @@ if __name__ == "__main__":
     tracker.start()
     logger.info("Iniciando el script...")
     start_time = time.time()
-    # 1. Descomentar para ejecutar desde la API
-    sumario = get_boe_sumario()
-    sumario_filtrado = filtrar_items(sumario)
-    pprint(sumario_filtrado)
-    # 2. Desccomentar para ejecutar la clasificación
-    clasificacion = classify_boe(sumario_filtrado)
-    pprint(clasificacion)
-    boe_paths = [value['url_pdf'] for value in filtrar_items(sumario).values()]
 
-    # 3. Descomentar para ejecutar desde el directorio
-    # Comentar las siguientes línea para ejecutar desde la API
+    # Parámetros de entrada
+    FECHA = time.strftime('%Y%m%d')
+
+    # Ejecución desde la API
+    sumario = get_boe_sumario(fecha=FECHA)
+    FECHA = None if not FECHA else time.strftime('%d/%m/%Y')
+    sumario_filtrado = filtrar_items(sumario)
+    boe_paths = [value['url_pdf'] for value in filtrar_items(sumario).values()]
+    # Fin ejecución desde la API
+
+    # clasificacion = classify_boe(sumario_filtrado)
+    # pprint(clasificacion)
+
+    # Ejecución con los PDFs locales.
     # boe_files = os.listdir("BOE\\PDF")
     # boe_paths = [os.path.join("BOE\\PDF", f) for f in boe_files]
-    
-    # sumario = get_boe_sumario(fecha="20250423")
-    # boe_paths = [value['url_pdf'] for value in filtrar_items(sumario).values()]
+    # Fin ejecución con los PDFs locales.
 
-
+    # Notificación
     summaries = generate_summaries_from_documents(boe_paths)
 
-    # Guardamos los resumenes en summaries.json
-    output_json_file = "summaries.json"
-    try:
-        with open(output_json_file, 'w', encoding='utf-8') as f:
-            json.dump(summaries, f, ensure_ascii=False, indent=4)
-        logger.info("Summaries saved to %s", output_json_file)
-    except Exception as e:
-        logger.error("Error saving summaries to JSON: %s", e)
+    save_summaries_to_file(summaries)
 
-    # Formatear el diccionario de resúmenes en una cadena para el cuerpo del email
-    email_body_parts = ["Resúmenes del BOE del día:\n\n"]
-    for url, summary_text in summaries.items():
-        if not isinstance(summary_text, str):
-            summary_text = str(summary_text)  # Convierte a cadena si no lo es
+    print(FECHA)
+    pprint(summaries)
+    mail_template = create_email_template(date=FECHA,
+                                          summaries=summaries,
+                                          depts=target_depts_to_string())
 
-        cleaned_summary = summary_text.strip()
-
-        email_body_parts.append(
-            f"URL: {url}\nResumen:\n{cleaned_summary}\n\n---\n")
-
-
-    email_body_string = "\n".join(email_body_parts)
-
-    # Enviamos el email con los resumenes diarios del BOE
-    receivers = read_json_receivers()
     send_boe_notification_email(
-        receivers=receivers,
-        body=email_body_string,
+        receivers=read_json_receivers(),
+        body=mail_template,
         attachment_paths=None,
         sender_email=remitente,
         password=clave
@@ -107,13 +92,3 @@ if __name__ == "__main__":
 
     logger.info("Emissions: %s kg CO₂eq", emissions)
     logger.info("Tiempo de ejecución: %.2f segundos", elapsed_time)
-
-# TODO:
-# Documentar el código (B)
-# Limpieza de código innecesario (A, B)
-# requirements.txt (B)
-# Ejecucion Codecarbon  (B)
-# README (A)
-# Contemplar casos de errores. (No hay BOE ese dia, no hay temática ese dia, se publican mas tarde,...) (A,B)
-# prompting para enfocar mejor la notificación. (A)
-# prompting para estandarizar el resumen del BOE. (B)
